3846d2da1130c6da5cfff235b4434397022c8e2b
SeungEon <js112099@gmail.com>
2023-03-27 02:00:03 +0900

    feat: 구글 로그인 버튼 구현

--- app/App.js
+++ app/App.js
@@ -1,6 +1,11 @@
 import { StatusBar } from "expo-status-bar";
-import React from "react";
+import React, { useEffect } from "react";
 import styled from "styled-components/native";
+import {
+  GoogleSignin,
+  GoogleSigninButton,
+} from "@react-native-google-signin/google-signin";
+import auth from "@react-native-firebase/auth";
 
 const Container = styled.View`
   flex: 1;
@@ -9,12 +14,32 @@ const Container = styled.View`
   justify-content: center;
 `;
 
+const onGoogleButtonPress = async () => {
+  await GoogleSignin.hasPlayServices();
+  const { idToken } = await GoogleSignin.signIn();
+  console.log(idToken);
+  const googleCredential = auth.GoogleAuthProvider.credential(idToken);
+  return auth().signInWithCredential(googleCredential);
+};
+
 const MainText = styled.Text``;
 
+const googleSigninConfigure = () => {
+  GoogleSignin.configure({
+    webClientId: "client-id",
+    offlineAccess: true,
+    forceCodeForRefreshToken: true,
+  });
+};
+
 export default function App() {
+  useEffect(() => {
+    googleSigninConfigure();
+  }, []);
+
   return (
     <Container>
-      <MainText>Open up App.js to start working on your app!</MainText>
+      <GoogleSigninButton onPress={() => onGoogleButtonPress()} />
     </Container>
   );
 }
