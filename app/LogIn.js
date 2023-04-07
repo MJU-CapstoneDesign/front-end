57e9d4b4336ad75f29e6433c3d9f5893a8a15076
SeungEon <js112099@gmail.com>
2023-04-08 01:08:55 +0900

    feat: jwt 토큰 저장

--- app/LogIn.js
+++ app/LogIn.js
@@ -5,7 +5,9 @@ import {
   GoogleSignin,
   GoogleSigninButton,
 } from "@react-native-google-signin/google-signin";
-import { iosClient, webClient } from "./Api";
+import { Button } from "react-native";
+import AsyncStorage from "@react-native-async-storage/async-storage";
+
 const Container = styled.View`
   flex: 1;
   background-color: #fff;
@@ -13,23 +15,49 @@ const Container = styled.View`
   justify-content: center;
 `;
 
+// 저장된 JWT 토큰을 검색합니다.
+let getToken = async () => {
+  try {
+    const value = await AsyncStorage.getItem("jwts");
+    if (value !== null) {
+      const data = JSON.parse(value)
+
+      return data;
+    } else {
+      console.log('저장된 JWT 토큰이 없습니다.');
+      return '';
+    }
+  } catch (error) {
+    console.log('JWT 토큰을 검색하는 동안 오류가 발생했습니다:', error);
+    return '';
+  }
+};
+
 const onGoogleButtonPress = async () => {
   await GoogleSignin.hasPlayServices();
   const { serverAuthCode } = await GoogleSignin.signIn();
   console.log(serverAuthCode);
 
-  await fetch("http://server-ip/auth/google/token", {
-    method: "POST",
-    body: serverAuthCode,
-  });
-};
+  await fetch("http://api-server:8080/login/GOOGLE/token?code=" + serverAuthCode, {
+    method: "GET",
+  }).then(data => data.json())
+  .then(async (tokens) => {
+    try {
+      console.log(">> tokens {}", tokens)
+      await AsyncStorage.setItem("jwts", JSON.stringify(tokens));
+      console.log('JWT 토큰이 저장되었습니다.');
+    } catch (error) {
+      console.log('JWT 토큰을 저장하는 동안 오류가 발생했습니다:', error);
+    }
+  })
+}
 
 const MainText = styled.Text``;
 
 const googleSigninConfigure = () => {
   GoogleSignin.configure({
-    webClientId: webClient,
-    iosClientId: iosClient,
+    webClientId: "847692404644-hms7b07e79r46p93pvta3q5jh5ru7d8o.apps.googleusercontent.com",
+    iosClientId: "847692404644-r5vm3f0m515r5f84h8au9tm804544due.apps.googleusercontent.com",
     offlineAccess: true,
     scopes: ["profile"],
   });
@@ -43,6 +71,18 @@ export default function LogIn() {
   return (
     <Container>
       <GoogleSigninButton onPress={() => onGoogleButtonPress()} />
+      <Button onPress={async () => {
+        const data = await getToken();
+        console.log(data);
+      }} title="getToken"/>
+      <Button onPress={async () => {
+        try {
+        await AsyncStorage.removeItem("jwts");
+        console.log("remove success")
+        } catch(error) {
+          console.log(error)
+        }
+      }} title="remove"/>
     </Container>
   );
 }
