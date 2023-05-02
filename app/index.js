0ad6d30f561c649bb3d45c95bb52f4cd84d931c2
SeungEon <js112099@gmail.com>
2023-05-03 00:51:07 +0900

    feat: 안드로이드 로컬 푸시 알람

--- app/index.js
+++ app/index.js
@@ -1,11 +1,14 @@
 import { registerRootComponent } from 'expo';
-import messaging from '@react-native-firebase/messaging'
+import messaging from '@react-native-firebase/messaging';
+import doPushNotification from './PushNotificationsConfig';
 
 import App from './App';
 
 messaging().setBackgroundMessageHandler(async message => {
-    console.log(message)
-  })
+	console.log(message);
+});
+
+doPushNotification();
 
 // registerRootComponent calls AppRegistry.registerComponent('main', () => App);
 // It also ensures that whether you load the danram in Expo Go or in a native build,
