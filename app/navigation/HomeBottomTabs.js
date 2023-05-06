a33152d10b06bfd8aa86ac874eb51f334462b5c4
YechanKim <rladPcks980@gmail.com>
2023-05-06 15:41:48 +0900

    feat:bottomTabsIconComplete

--- app/navigation/HomeBottomTabs.js
+++ app/navigation/HomeBottomTabs.js
@@ -5,17 +5,99 @@ import Chatting from "../screens/Home/Chatting";
 import Add from "../screens/Home/Add";
 import More from "../screens/Home/More";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import { Octicons } from "@expo/vector-icons";
+import { MaterialCommunityIcons } from "@expo/vector-icons";
+import { MaterialIcons } from "@expo/vector-icons";
+import { Ionicons } from "@expo/vector-icons";
+import { Feather } from "@expo/vector-icons";
 
 const Tab = createBottomTabNavigator();
 
 export default function HomeBottomTabs() {
   return (
-    <Tab.Navigator screenOptions={{ headerShown: false }}>
-      <Tab.Screen name="HomeTabs" component={HomeTabs} />
-      <Tab.Screen name="Calendar" component={Calendar} />
-      <Tab.Screen name="Add" component={Add} />
-      <Tab.Screen name="Chatting" component={Chatting} />
-      <Tab.Screen name="More" component={More} />
+    <Tab.Navigator
+      screenOptions={{ headerShown: false, tabBarShowLabel: false }}
+      tabBarOptions={{
+        activeTintColor: "black",
+        inactiveTintColor: "gray",
+      }}
+    >
+      <Tab.Screen
+        name="HomeTabs"
+        component={HomeTabs}
+        options={{
+          tabBarIcon: ({ focused }) =>
+            focused ? (
+              <Octicons name="home" size={21} color="black" />
+            ) : (
+              <Octicons name="home" size={21} color="gray" />
+            ),
+        }}
+      />
+      <Tab.Screen
+        name="Calendar"
+        component={Calendar}
+        options={{
+          tabBarIcon: ({ focused }) =>
+            focused ? (
+              <MaterialCommunityIcons
+                name="calendar-check-outline"
+                size={24}
+                color="black"
+              />
+            ) : (
+              <MaterialCommunityIcons
+                name="calendar-check-outline"
+                size={24}
+                color="gray"
+              />
+            ),
+        }}
+      />
+      <Tab.Screen
+        name="Add"
+        component={Add}
+        options={{
+          tabBarIcon: ({ focused }) =>
+            !focused ? (
+              <MaterialIcons name="add-circle-outline" size={40} color="red" />
+            ) : (
+              <MaterialIcons name="add-circle" size={40} color="red" />
+            ),
+        }}
+      />
+      <Tab.Screen
+        name="Chatting"
+        component={Chatting}
+        options={{
+          tabBarIcon: ({ focused }) =>
+            focused ? (
+              <Ionicons
+                name="ios-chatbox-ellipses-outline"
+                size={24}
+                color="black"
+              />
+            ) : (
+              <Ionicons
+                name="ios-chatbox-ellipses-outline"
+                size={24}
+                color="gray"
+              />
+            ),
+        }}
+      />
+      <Tab.Screen
+        name="More"
+        component={More}
+        options={{
+          tabBarIcon: ({ focused }) =>
+            focused ? (
+              <Feather name="more-horizontal" size={24} color="black" />
+            ) : (
+              <Feather name="more-horizontal" size={24} color="gray" />
+            ),
+        }}
+      />
     </Tab.Navigator>
   );
 }
